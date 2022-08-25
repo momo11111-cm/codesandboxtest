e2b1032c87e4e62cb4f0ac94110a7070b71a4032
momo11111-cm <112041227+momo11111-cm@users.noreply.github.com>
2022-08-25 17:14:40 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// /**
+//  * const.let変数宣言
+//  */
+// var val1 = "var変数";
+// console.log(val1);
+// //var変数の上書き
+// // val1=
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// //const変数は上書きも再宣言は不可能
+//プロパティ変更可
+// const val4 = {
+//   name: "あ",
+//   age: 28
+// };
+// console.log(val4);
+
+// val4.name = "a";
+// val4.address = "chiba";
+// console.log(val4);
+//配列
+// // const val5 = ["dog", "cat"];
+// // val5[0]='bird';
+// // console.log(val5);
+
+// const name = "lisa";
+// const age = 1;
+
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+
+//アロー関数
+
+//分割代入
+
+// const myProfile={
+//   name:'ああ',
+//   age:1,
+// };
+// const message1=`名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+// const{name, age}=myProfile
+// const message2=`名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+const myProfile=['あ',1];
+
+const message3=`名前は${myProfile[0]です。年齢は${myProfile[1]}です。`;
+console.log(message3);
